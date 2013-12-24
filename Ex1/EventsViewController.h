2090a8d4ec1cfd8ae81c518052f08a5cc19c8352
Xurxo Méndez Pérez <sonxurxo@gmail.com>
2013-12-24 13:31:52 +0100

    GestureRecognizers en IB

---
+++ Ex1/EventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsViewController.h
+//  Ex1
+//
+//  Created by Xurxo Méndez Pérez on 24/12/13.
+//  Copyright (c) 2013 SmartGalApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsViewController : UIViewController
+
+@end
---
+++ Ex1/EventsViewController.m
@@ -0,0 +1,38 @@
+//
+//  EventsViewController.m
+//  Ex1
+//
+//  Created by Xurxo Méndez Pérez on 24/12/13.
+//  Copyright (c) 2013 SmartGalApps. All rights reserved.
+//
+
+#import "EventsViewController.h"
+
+@interface EventsViewController ()
+
+@end
+
+@implementation EventsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
